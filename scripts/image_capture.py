47fc580596b1c4ecb5489c593e8e7dd89c33744d
GitYangFan <103840315+GitYangFan@users.noreply.github.com>
2023-06-14 18:34:23 -0700

    Update image_capture.py

--- scripts/image_capture.py
+++ scripts/image_capture.py
@@ -1,15 +1,18 @@
 import rospy
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+import numpy as np
+#from cv_bridge import CvBridge
 import jetson_utils
 
 img = None
+#bridge = CvBridge()
 
 def image_callback(msg):
     global img
-    bridge = CvBridge()
-    img_cv = bridge.imgmsg_to_cv2(msg, "bgr8")  # convert 'Image' to 'numpy' array
-    img = jetson_utils.cudaFromNumpy(img_cv)    # convert 'numpy' array to cuda img for using DNN model
+    # img_cv = bridge.imgmsg_to_cv2(msg, "bgr8")
+    img_cv = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
+    img = jetson_utils.cudaFromNumpy(img_cv)
+
 
 def get_image():
     rospy.init_node('image_capture')
@@ -19,9 +22,14 @@ def get_image():
         if img is not None:
             print('Image captured successfully.')
             return img
+        else:
+            print('failed to capture image.')
         rospy.sleep(0.1)
 
 
 """
 -------------------- test ----------------------------
 """
+if __name__ == '__main__':
+    image = get_image()
+    print(image)
