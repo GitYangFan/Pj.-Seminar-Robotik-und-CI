263ebe9ebaedf2e2f83dca60a5ff95637f7cbb40
GitYangFan <103840315+GitYangFan@users.noreply.github.com>
2023-06-30 05:19:53 -0700

    Update object_localization.py

--- scripts/object_localization.py
+++ scripts/object_localization.py
@@ -133,19 +133,26 @@ def get_object_position():
              np.cos(orientation_jetbot[2] + object_angle[i][0]))]
         # object_position[i] = [object_position[i][1], object_position[i][0]]
         # find the cube within the base area
+        flag = False
         for base in bases:
             if math.sqrt((object_position[i][0] - base[0]) ** 2 + (object_position[i][1] - base[1]) ** 2) < 0.1:
                 idx_cube_remove.append(i)
+                flag = True
+                break
+        if flag:
+            continue
         if object_score[i] < 0.7:
             print('score is too low!',object_score[i])
             idx_cube_remove.append(i)
+            continue
         if (object_size[i][0] > 1.5 * object_size[i][1]):
             print('It is not a cube',object_size[i])
             idx_cube_remove.append(i)
+            continue
         print('detected:', object_name[i], 'possibility:', object_score[i], 'position:', object_position[i],'distance_horizon:', object_distance_horizon[i], 'angle:', object_angle[i][0] / np.pi * 180)
         print('------------split---------------')
     # remove the cube within the base area
-    if len(idx_cube_remove)!=0:
+    if idx_cube_remove!=[]:
         for i in range(len(idx_cube_remove) - 1, -1, -1):
             print('remove the object within the base area:',object_name[idx_cube_remove[i]],object_position[idx_cube_remove[i]])
             del object_name[idx_cube_remove[i]]
@@ -175,27 +182,34 @@ def get_objects():
              np.cos(orientation_jetbot[2] + object_angle[i][0]))]
         # object_position[i] = [object_position[i][1], object_position[i][0]]
         # find the cube within the base area
+        flag = False
         for base in bases:
             if math.sqrt((object_position[i][0] - base[0]) ** 2 + (object_position[i][1] - base[1]) ** 2) < 0.1:
                 idx_cube_remove.append(i)
+                flag = True
+                break
+        if flag:
+            continue
         if object_score[i] < 0.7:
             print('score is too low!',object_score[i])
             idx_cube_remove.append(i)
+            continue
         if (object_size[i][0] > 1.5 * object_size[i][1]):
             print('It is not a cube',object_size[i])
             idx_cube_remove.append(i)
+            continue
         print('detected:', object_name[i], 'possibility:', object_score[i], 'position:', object_position[i],
               'distance_horizon:', object_distance_horizon[i], 'angle:', object_angle[i][0] / np.pi * 180)
         print('------------split---------------')
-        # remove the cube within the base area
-        if len(idx_cube_remove)!=0:
-            for i in range(len(idx_cube_remove) - 1, -1, -1):
-                print('remove the object within the base area:', object_name[idx_cube_remove[i]],
-                      object_position[idx_cube_remove[i]])
-                del object_name[idx_cube_remove[i]]
-                del object_score[idx_cube_remove[i]]
-                del object_position[idx_cube_remove[i]]
-                del object_distance_horizon[idx_cube_remove[i]]
+    # remove the cube within the base area
+    if idx_cube_remove!=[]:
+        print('idx_cube_remove:',idx_cube_remove)
+        for i in range(len(idx_cube_remove) - 1, -1, -1):
+            print('remove the object within the base area:', object_name[idx_cube_remove[i]],object_position[idx_cube_remove[i]])
+            del object_name[idx_cube_remove[i]]
+            del object_score[idx_cube_remove[i]]
+            del object_position[idx_cube_remove[i]]
+            del object_distance_horizon[idx_cube_remove[i]]
     for i in range(len(object_name)):
         obj = ObjecT(object_name[i], object_score[i], object_position[i], object_distance_horizon[i])
         objects.append(obj)
@@ -205,7 +219,7 @@ def get_objects():
 """
 test 2
 """
-
+"""
 # this node initialization can only be called once !! So it should be writen in main function
 rospy.init_node('object_localization')
 object_name, object_score, object_position, object_distance_horizon = get_object_position()
@@ -215,7 +229,7 @@ print('detected:', object_name, 'possibility:', object_score, 'object position:'
 #print('object class detected:',objects)
 
 # print('distance list:', object_distance, 'angle list:', object_angle)
-
+"""
 
 """
 
