4999654d19acdb55e6e2e56abcb56e647f027194
GitYangFan <103840315+GitYangFan@users.noreply.github.com>
2023-07-16 18:41:39 +0200

    Update stop.py

--- scripts/stop.py
+++ scripts/stop.py
@@ -3,6 +3,8 @@ from basic_move import *
 from object_localization import *
 from motors_waveshare import MotorControllerWaveshare
 
+# emergency stop for testing
+
 jetbot_motor = MotorControllerWaveshare()
 #rospy.init_node('object_localization')
 stop(jetbot_motor)
